b27320c24930a8f289cc7ee36fc054f6d9ce7351
ambaker1 <ambaker1@mtu.edu>
2021-11-02 21:16:10 -0400

    Bug fix
    It didn't like spaces in path names. Should be fine now.

--- OpenSeesMPI.bat
+++ OpenSeesMPI.bat
@@ -3,12 +3,12 @@ SET batdir=%~dp0
 REM Split input arguments for application (last arg is input file)
 SET mpiargs=
 :loop
-SET lastarg=%1
+SET lastarg=%~1
 SHIFT
-IF "%1"=="" GOTO continue
-SET mpiargs=%mpiargs% %lastarg%
+IF "%~1"=="" GOTO continue
+SET mpiargs=%mpiargs% "%lastarg%"
 GOTO loop
 :continue
 REM Dummy call to OpenSees to get header, then call in parallel with opsmpi.
-OpenSees %batdir%\opsmpi.tcl
-mpiexec %mpiargs% OpenSees %batdir%\opsmpi.tcl %lastarg% 2>NUL
+OpenSees "%batdir%\opsmpi.tcl"
+mpiexec %mpiargs% OpenSees "%batdir%\opsmpi.tcl" "%lastarg%" 2>NUL
